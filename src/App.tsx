3ea2a4d64792e34146af838a5764a22c0b9b73b4
Vincent Brodin <vincent.brodin21@gmail.com>
2025-11-03 01:02:05 +0100

    Made mirror slimmer

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import WeatherModule from "./components/WeatherModule"
 
 function App() {
   return (
-    <div className='w-screen h-screen grid grid-cols-3 grid-rows-3 bg-black text-white'>
+    <div className='w-screen h-screen grid grid-cols-2 grid-rows-3 bg-black text-white'>
       <ClockModule className="row-start-1 col-start-1" />
-      <WeatherModule className="row-start-1 col-start-3" lat={59.6191} lon={17.7234} />
+      <WeatherModule className="row-start-1 col-start-2" lat={59.6191} lon={17.7234} />
     </div>
   )
 }
